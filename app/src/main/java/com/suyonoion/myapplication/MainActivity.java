c9c2e22fa980fef5d25b51d90ca64be8d95ab3a8
suyonoion <suyonoion8@gmail.com>
2015-09-12 22:26:36 +0700

    Mengubah menjadi CustomView DinamicLayout

--- app/src/main/java/com/suyonoion/myapplication/MainActivity.java
+++ app/src/main/java/com/suyonoion/myapplication/MainActivity.java
@@ -1,9 +1,7 @@
 package com.suyonoion.myapplication;
 
 import android.app.Activity;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 
 
 public class MainActivity extends Activity {
@@ -11,9 +9,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SharedPreferences ganti_pref = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-        final String ganti_str = ganti_pref.getString("pilihLayout", "layout1");
-        setContentView(setResource(ganti_str,"layout"));
+        setContentView(setResource("main", "layout"));
+
     }
 
     public void onResume() {
